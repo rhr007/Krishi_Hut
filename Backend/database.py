fd4437f2f507e6866340c5445f920d78042c34ac
IftekharHashib2001012 <iftekharhashib68@gmail.com>
2024-11-17 10:05:04 +0600

    managing new branch

--- Backend/database.py
+++
@@ -1,9 +0,0 @@
-from sqlmodel import create_engine, Session
-
-DATABASE_URL = "sqlite:///./database.db"
-
-engine = create_engine(DATABASE_URL)
-
-def get_db():
-    with Session(engine) as database:
-        yield database
