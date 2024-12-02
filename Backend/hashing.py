1dc98772afbaa3ad73d222ea350b4afd8cdd6505
Rakibul Hasan Rakib <rhr2001007@gmail.com>
2024-12-02 18:44:57 +0600

    Add comment to password hashing

--- Backend/hashing.py
+++ Backend/hashing.py
@@ -6,4 +6,4 @@ def get_hashed_password(plain_password):
     return pwd_context.hash(plain_password)
 
 def verify_password(plain_passowrd, hashed_password):
-    return pwd_context.verify(plain_passowrd, hashed_password)
+    return pwd_context.verify(plain_passowrd, hashed_password) #check the passwrod
