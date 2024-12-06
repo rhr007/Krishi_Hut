9699de6044d3a88d4ce30c4c467a840c5d52eb8a
Rakibul Hasan Rakib <rhr2001007@gmail.com>
2024-12-06 15:56:34 +0600

    add home component to Routes

--- Frontend/krishi-hut-frontend/src/App.jsx
+++ Frontend/krishi-hut-frontend/src/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import Navbar from './components/Navbar';
 
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './components/Home'
+
 const App = () => {
   return (
     <>
       <Navbar />
+
       <Routes>
-        <Route path='home' element={}>
+        <Route path='home' element={<Home />} />
       </Routes>
+
     </>
   );
 }
