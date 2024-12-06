acb4fe0f505f55e40082972f38eefef225889a2f
RAFIUL RABBI ZAHID <rafiulofficial18@gmail.com>
2024-12-06 16:14:17 +0600

    added signup routes and component

--- Frontend/krishi-hut-frontend/src/App.jsx
+++ Frontend/krishi-hut-frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './components/Home'
+import SignUp from './components/SignUp'
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
 
       <Routes>
         <Route path='home' element={<Home />} />
+        <Route path='signup' element={<SignUp />} />
       </Routes>
 
     </>
